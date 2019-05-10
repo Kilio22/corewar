e9b24764ea54cd907e345ef78437dcd45578e551
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-10 15:22:56 +0200

    Fixed comment parsing

--- asm/src/create_instruction_from_line.c
+++ asm/src/create_instruction_from_line.c
@@ -12,8 +12,9 @@ static int add_node(struct instruction *inst, char *str_inst, short id)
     struct instruction *last = get_last_inst_node(inst);
     int t_index = find_token_index(str_inst);
 
+    if (GET_TOKEN_TYPE(t_index) == D_COMMENT)
+        return free(str_inst), 0;
     if (!last->instruction) {
-        last->instruction = str_inst;
         if (id == ID_LABEL && GET_TOKEN_ID(t_index) == ID_NOTHING)
             last->id = ID_LABEL;
         else
@@ -23,9 +24,9 @@ static int add_node(struct instruction *inst, char *str_inst, short id)
         if (!last->next)
             return -1;
         last = last->next;
-        last->instruction = str_inst;
         last->id = GET_TOKEN_ID(t_index);
     }
+    last->instruction = str_inst;
     last->type = GET_TOKEN_TYPE(t_index);
     return 0;
 }
@@ -39,10 +40,8 @@ static int add_instruction(struct instruction *inst, char **line, int t_index,
         str_inst = slice_string(line, i);
         if (!str_inst)
             return -1;
-        if (my_strlen(str_inst) == 0) {
-            free(str_inst);
-            return 0;
-        }
+        if (my_strlen(str_inst) == 0)
+            return free(str_inst), 0;
     } else
         str_inst = cut_string(line, i);
     if (!str_inst)
@@ -50,6 +49,8 @@ static int add_instruction(struct instruction *inst, char **line, int t_index,
     if (my_strlen(str_inst) == 0) {
         free(str_inst);
         str_inst = my_strndup(*line, 1);
+        if (!str_inst)
+            return -1;
         (*line)++;
     }
     return add_node(inst, str_inst, GET_TOKEN_ID(t_index));
