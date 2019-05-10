bc98c01aa5caf4f192cea835664a5f60c5c94108
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-10 16:00:22 +0200

    Main prototype added

---
+++ corewar/src/main.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** corewar
+** File description:
+** main
+*/
+
+static int corewar_main(void)
+{
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    (void) argv;
+    if (argc < 2)
+        return 84;
+    return corewar_main();
+}
