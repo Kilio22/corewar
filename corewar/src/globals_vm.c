cee85e8d7142f0f5c335ce1b1f93c43279a11372
Kylian Balan <kylian.balan@epitech.eu>
2019-05-11 16:35:22 +0200

    add globals_parsing file

--- corewar/src/globals_vm.c
+++ corewar/src/globals_vm.c
@@ -7,13 +7,3 @@
 
 #include <stdlib.h>
 #include "corewar.h"
-
-const char *flags[] = {
-    "-dump",
-    "-n",
-    "-a"
-};
-
-int (* const tab_ftcs[])(size_t *, char const **argv, parsing_t *, int *) = {
-        manage_dump, manage_nb, manage_adress
-};
---
+++ corewar/src/parsing/parsing_globals.c
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** corewar
+** File description:
+** parsing_globals
+*/
+
+#include "corewar.h"
+
+const char *flags[] = {
+    "-dump",
+    "-n",
+    "-a"
+};
+
+int (* const tab_ftcs[])(size_t *, char const **argv, parsing_t *, int *) = {
+        manage_dump, manage_nb, manage_adress
+};
