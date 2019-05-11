a69744debce0b843205e74bdc4ea8f307c0d2795
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-11 16:35:50 +0200

    Added blank files (for now)

---
+++ corewar/src/init_arena/create_arena.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2019
+** corewar
+** File description:
+** create_arena
+*/
+
+#include <unistd.h>
+#include "corewar.h"
+
+// char *create_arena(champion_t *champions[MAX_CHAMPIONS + 1])
+// {
+//     char *arena = malloc(sizeof(char) * MEM_SIZE);
+
+//     if (!arena)
+//         return NULL;
+//     for (int i = 0; champions[i]; i++) {
+//         if (read(champions[i]->fd, champions[i]->beginning,
+// champions[i]->prog_size) != champions[i]->prog_size) {
+//             free
+//         }
+//     }
+//     return arena;
+// }
---
+++ corewar/src/init_arena/create_champion_list.c
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2019
+** corewar
+** File description:
+** create_champion_list
+*/
+
+#include "corewar.h"
+
+
