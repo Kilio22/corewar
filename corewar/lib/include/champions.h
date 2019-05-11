6da5562952737a213bce34f9fb5289201bd0e439
Kylian Balan <kylian.balan@epitech.eu>
2019-05-11 16:18:45 +0200

    add parsing main fct

--- corewar/lib/include/champions.h
+++ corewar/lib/include/champions.h
@@ -12,11 +12,23 @@
 
 typedef struct champion {
     char prog_name[PROG_NAME_LENGTH + 1];
-    char *bytecode;
+    char *start_ptr;
     int prog_size;
     int reg[REG_NUMBER];
     int pc;
     int carry;
 } chamption_t;
+
+typedef struct champion_params {
+    char *fp;
+    int nb;
+    int adress;
+} champion_params_t;
+
+typedef struct parsing {
+    int dump;
+    int nb_used[4];
+    champion_params_t **params;
+} parsing_t;
 
 #endif /* !CHAMPIONS_H_ */
--- corewar/lib/include/corewar.h
+++ corewar/lib/include/corewar.h
@@ -16,4 +16,16 @@
 #include "my_string.h"
 #include "champions.h"
 
+champion_params_t **realloc_params(champion_params_t **old);
+int check_flag(char const *str);
+int manage_adress(size_t *i, char const **tab, parsing_t *parsing, int *current);
+int manage_nb(size_t *i, char const **tab, parsing_t *parsing, int *current);
+int manage_dump(size_t *i, char const **tab, parsing_t *parsing, int *current);
+int add_champ(char const *fp, parsing_t *parsing, int *current);
+int parse_args(int ac, char const *argv[], parsing_t *parsing);
+void destroy_args(parsing_t *parsing);
+
+extern const char *flags[];
+extern int (* const tab_ftcs[])(size_t *, char const **argv, parsing_t *, int *);
+
 #endif /* !COREWAR_H_ */
