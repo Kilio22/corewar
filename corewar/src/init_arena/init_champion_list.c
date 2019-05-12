95b75c6101e157fe98e70de0af9c8262d48f303c
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-05-12 11:28:11 +0200

    Every champion is created through init_champion_list()

--- corewar/src/init_arena/init_champion_list.c
+++ corewar/src/init_arena/init_champion_list.c
@@ -7,10 +7,16 @@
 */
 
 #include "corewar.h"
-/*
-int init_champion_list(champion_t *champions[MAX_CHAMPIONS + 1])
-{
 
+int init_champions(champion_t **champions, parsing_t *rules)
+{
+    for (int i = 0; rules->params[i]; i++) {
+        champions[i] = create_champion(rules->params[i]->fp);
+        if (!champions[i])
+            return 84;
+        champions[i]->prog_id = rules->params[i]->nb;
+        champions[i]->reg[0] = champions[i]->prog_id;
+        champions[i]->pc = rules->params[i]->adress % MEM_SIZE;
+    }
     return 0;
 }
- */
--- corewar/src/main.c
+++ corewar/src/main.c
@@ -10,7 +10,7 @@
 #include <unistd.h>
 #include "corewar.h"
 
-/* static void print_params(parsing_t *parsing)
+static void print_params(parsing_t *parsing)
 {
     for (int i = 0; parsing->params[i]; i++) {
         my_printf("------NEW-----\n");
@@ -18,19 +18,23 @@
         my_printf("Adress: %d\n", parsing->params[i]->adress);
         my_printf("Nb champ: %d\n", parsing->params[i]->nb);
     }
-} */
+}
 
 static int corewar_main(int ac, char const *argv[])
 {
     parsing_t parsing;
-    //champion_t *champions[MAX_CHAMPIONS + 1] = {0, 0, 0, 0, 0};
+    champion_t *champions[MAX_CHAMPIONS + 1] = {0, 0, 0, 0, 0};
+    int n_return = 0;
 
     if (parse_args(ac, argv, &parsing) == -1) {
         destroy_args(&parsing);
         return 84;
     }
+    print_params(&parsing);
+    n_return = init_champions(champions, &parsing);
+    destroy_champions(champions);
     destroy_args(&parsing);
-    return 0;
+    return n_return;
 }
 
 int main(int argc, char const *argv[])
