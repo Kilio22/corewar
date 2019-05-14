a04f7a7c0cf7439044d1d0c695da912c4d613f55
Bellanger Nathan <bellanger.nathan@epitech.eu>
2019-05-14 13:52:31 +0200

    [FEATURE] : inst_sub

---
+++ corewar/src/instruction/inst_sub.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2019
+** inst_sub.c
+** File description:
+** sub instruction
+*/
+
+#include "corewar.h"
+
+static int is_reg(int reg)
+{
+    if (reg < 0 || reg > 15)
+        return -1;
+    return 0;
+}
+
+int inst_sub(champion_t *champ, char *arena __attribute__((unused)),
+char desc, int *args)
+{
+    for (int i = 0; i < 3; i++)
+        if (is_reg(args[i]) == -1)
+            return -1;
+    champ->reg[args[2] - 1] = champ->reg[args[0] - 1] - champ->reg[args[1] - 1];
+    refresh_carry(champ, champ->reg[args[2] - 1]);
+    return 0;
+}
