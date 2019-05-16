6ecf4ad76ff3cf82977fb5749c60727b4631d955
Kylian Balan <kylian.balan@epitech.eu>
2019-05-16 13:20:15 +0200

    correct is game ended

--- corewar/src/is_game_ended.c
+++ corewar/src/is_game_ended.c
@@ -17,14 +17,14 @@ void display_winning_champions(champion_t **champions)
     }
     nb = get_champion_live(0);
     for (int i = 1; champions[i]; i++) {
-        if (champions[i]->cycles_until_death == -1 ||
+        if (champions[i]->forked == false &&
 get_champion_live(champions[i]->prog_id) > nb)
             nb = get_champion_live(champions[i]->prog_id);
     }
     for (int i = 0; champions[i]; i++) {
-        if (champions[i]->cycles_until_death == -1 ||
+        if (champions[i]->forked == false &&
 get_champion_live(champions[i]->prog_id) == nb)
-            my_printf("The player%d (%s) won.\n",
+            my_printf("The player %d (%s) won.\n",
 champions[i]->prog_id, champions[i]->prog_name);
     }
 }
@@ -34,7 +34,7 @@ bool is_game_ended(champion_t **champions)
     int nb_alive = 0;
 
     for (int i = 0; champions[i]; i++) {
-        if (champions[i]->cycles_until_death == -1 ||
+        if (champions[i]->forked == false &&
 get_champion_live(champions[i]->prog_id) > 0)
             nb_alive++;
     }
@@ -47,7 +47,7 @@ void update_champions_live_status(champion_t **champions)
 {
     update_champions_live();
     for (int i = 0; champions[i]; i++) {
-        if (champions[i]->cycles_until_death == -1 ||
+        if (champions[i]->forked == true||
 get_champion_live(champions[i]->prog_id) > 0)
             continue;
         delete_champion_id(champions, champions[i]->prog_id);
