3cca7e9d6162b185298a5e9d0c06d2e8e896988d
HeyShafty <nathan.lecorchet@epitech.eu>
2019-05-20 17:30:23 +0200

    Whoops st aha

--- corewar/src/instruction/inst_st.c
+++ corewar/src/instruction/inst_st.c
@@ -9,12 +9,17 @@
 
 int inst_st(champion_t *champ, core_t *core, code_t desc, int *args)
 {
+    int pos;
+
     if (is_reg(desc, args, 2) == -1)
         return -1;
     if (ARG_TYPE(1) == REG)
         champ->reg[args[1] - 1] = GET_VAL(0);
-    else
-        write_arg(core->arena,
-(champ->pc + (args[1] % IDX_MOD)) % MEM_SIZE, GET_VAL(0));
+    else {
+        pos = (champ->pc + (args[1] % IDX_MOD)) % MEM_SIZE;
+        while (pos < 0)
+            pos += MEM_SIZE;
+        write_arg(core->arena, pos, GET_VAL(0));
+    }
     return 0;
 }
